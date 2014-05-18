4463cb03bd7a9bf1ee235ea024028f2b06a0255a
Marc-Olivier Arsenault <marcolivier.arsenault@gmail.com>
2014-05-18 02:53:11 -0400

    Menu is back and compiling

--- src/org/arsenaultmarc45/polarheartmonitor/MainActivity.java
+++ src/org/arsenaultmarc45/polarheartmonitor/MainActivity.java
@@ -12,6 +12,7 @@ import android.bluetooth.BluetoothDevice;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -71,6 +72,16 @@ public class MainActivity extends Activity  implements OnItemSelectedListener {
 		spinner1.setAdapter(dataAdapter);
 	}
 
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 	
 	/**
 	 * A placeholder fragment containing a simple view.
