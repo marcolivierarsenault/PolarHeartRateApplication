263bcd10a157bc70ac77abb0ac30d03d8940c0f4
Marc-Olivier Arsenault <marcolivier.arsenault@gmail.com>
2014-05-18 01:35:57 -0400

    Drop down list work

--- src/org/arsenaultmarc45/polarheartmonitor/MainActivity.java
+++ src/org/arsenaultmarc45/polarheartmonitor/MainActivity.java
@@ -1,5 +1,8 @@
 package org.arsenaultmarc45.polarheartmonitor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.AlertDialog;
@@ -11,9 +14,16 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 import android.os.Build;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity  implements OnItemSelectedListener {
+	
+	int i =0;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,21 @@ public class MainActivity extends Activity {
 			getFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
+
+	}
+	
+	public void onStart(){
+		super.onStart();
+		Spinner spinner1 = (Spinner) findViewById(R.id.spinner1);
+		List<String> list = new ArrayList<String>();
+		list.add("list 1");
+		list.add("list 2");
+		list.add("list 3");
+		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+			android.R.layout.simple_spinner_item, list);
+		dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spinner1.setOnItemSelectedListener(this);
+		spinner1.setAdapter(dataAdapter);
 	}
 
 	
@@ -42,6 +67,20 @@ public class MainActivity extends Activity {
 					container, false);
 			return rootView;
 		}
+	}
+
+
+	@Override
+	public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2,
+			long arg3) {
+		//ID est celui en ordre
+		
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+
+		
 	}
 
 }
